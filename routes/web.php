3a93c705da5bee07892ea86015852e07e2791992
Manuel Guido <manuelguido.gb@gmail.com>
2026-06-19 13:11:50 -0300

    feat(routes): define routes for webhook inspection and capture functionality

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\Webhooks\CaptureWebhookController;
+use App\Http\Controllers\Webhooks\WebhookInspectorController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Welcome')->name('home');
+Route::get('/', [WebhookInspectorController::class, 'index'])->name('home');
+
+Route::get('/inspector/bin', [WebhookInspectorController::class, 'bin'])->name('inspector.bin');
+Route::get('/inspector/{bin:token}/requests', [WebhookInspectorController::class, 'requests'])->name('inspector.requests');
+Route::delete('/inspector/{bin:token}/requests', [WebhookInspectorController::class, 'clear'])->name('inspector.requests.clear');
+
+Route::match(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], '/hook/{token}', CaptureWebhookController::class)
+    ->middleware('throttle:webhook-capture')
+    ->name('webhooks.capture');
