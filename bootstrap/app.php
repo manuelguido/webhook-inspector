51b015020a1a4fae2b218a8b55d172531dd318a9
Manuel Guido <manuelguido.gb@gmail.com>
2026-06-19 13:10:47 -0300

    feat(middleware): add CSRF token validation exception for webhook routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+            'hook/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
